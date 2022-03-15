b7b1cc42c25f25955243fdc617cfdff4c1b5d44c
fernanda dias silva <f.dias.da.silva@gmail.com>
2022-03-15 12:24:19 -0300

    :tada: projeto finalizado

---
+++ src/database/db.js
@@ -0,0 +1,87 @@
+//Configuração do nosso banco de dados
+//Importar a depedencia do sqlite3, e irá retornar um objeto:
+//Dica: uma função dentro de um objeto se chama METODO
+const sqlite3 = require("sqlite3").verbose() //Verbose: quero ver uma mensagem no terminal toda vez que acontecer algo e retorna um objeto
+
+//Criar o objeto que irá fazer operações no Banco de Dados:
+//NEW inicia um novo objeto dentro de uma classe
+const db = new sqlite3.Database("./src/database/database.db") //Irá criar um banco de dados no caminho que esta entre ()
+
+//Vamos exportar o objeto db
+module.exports = db
+
+/*
+//Utilizar o objeto de banco de dados para nossas operações
+db.serialize(() => { //Ira rodar uma sequencia de códigos - db é um objeto
+    
+    //com comandos sql:
+    //1-Criar uma tabela: (abaixo é um tamplate litius)
+    db.run(`
+        CREATE TABLE IF NOT EXISTS places (
+            id INTEGER PRIMARY KEY AUTOINCREMENT,
+            image TEXT,
+            name TEXT,
+            address TEXT,
+            address2 TEXT,
+            state TEXT,
+            city TEXT,
+            items TEXT
+        );    
+    `)
+
+    //2-Inserir dados na tabela 
+    //Primeiro coloca os campos "places ()" e depois os valores "values ()"
+    const query = `
+    INSERT INTO places (
+        image, 
+        name,
+        address,
+        address2,
+        state,
+        city,
+        items
+    ) VALUES (?,?,?,?,?,?,?);
+    `
+    const values = [
+        "https://images.unsplash.com/photo-1567393528677-d6adae7d4a0a?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1950&q=80",
+        "Papersider",
+        "Guilherme Gemballa, Jardim América",
+        "Número 260",
+        "Santa Catarina",
+        "Rio do Sul",
+        "Resíduos Eletrônicos Lâmpadas"
+    ]
+
+    function afterInsertData(err) {
+        if(err) {
+            return console.log(err)
+        }
+        console.log("Cadastrado com sucesso")
+        console.log(this)
+    }
+
+    //afterInsertData é uma callback
+    //db.run(query, values, afterInsertData)
+
+
+    //3-Consultar os dados da tabela PLACES (* -> significa acessar todos os campos da tabela)
+    //Se fosse trazer só os nomes does registros:  db.all(`SELECT name FROM places`, function(err, rows)
+    //db.all(`SELECT * FROM places`, function(err, rows){
+    //    if(err) { //Esse é o tratamento do caminho do erro
+    //        return console.log(err)
+    //    }
+    //    console.log("Aqui estão seus registros: ")
+    //    console.log(rows)
+    //})
+
+    //4-Deletar um dado da tabela
+    //Quando se usa uma ?, será colocado uma sequencia de interrogações. Nesse caso o valor dela é 1, pois só há um registro e o id é 1
+    //Explicação: Irá deletar da tabela onde o id é 1, e depois rodar um função callback para tratar erro
+    //Para deletar algum dado, basta adicionar o id que quer deletar e tirar do comentário (47:12 do video)
+    db.run(`DELETE FROM places WHERE id = ?`, [4], function(err) {
+      if(err) { 
+            return console.log(err)
+       }
+       console.log("Registro deletado com sucesso!")
+    })
+})*/
--- src/server.js
+++ src/server.js
@@ -1,27 +1,109 @@
-const express = require('express')
-const server = express()
+const express = require("express") //Iniciar o servidor, irá pedir o express e salvar nessa variável
+const server = express()//Executar o express
 
-//configurar pasta publication
-server.use(express.static("public"))
+//Pegar o banco de dados
+const db = require("./database/db")
 
-//usar template engine
-const nunjucks = require('nunjucks')
-nunjucks.configure("src/views", {
-    express: server,
-    moCache: true
+//Configuração da pasta public (.use é para isso, fazer a configuração)
+server.use(express.static("public")) //Configuração da pasta public para que apareça as paginas dentro dela
+
+//O express não roda o campo body da page, portanto:
+//Habilitar o uso do req.body na nossa aplicação
+server.use(express.urlencoded({ extended: true }))
+
+//Utilizando template engine
+const nunjucks = require("nunjucks") //Pedindo uma depedencia ja instalado no npm
+nunjucks.configure("src/views", { //Pasta de html para configurarmos
+    express: server, //Ligamos o nunjucks ao express, dessa maneira as rotas funcionam: res.render("index.html")
+    noCache: true//Sem cache
 })
 
-//configurar caminhos da minha aplicacao
-server.get('/', (req, res) => {
- return res.render('index.html')
-})
-server.get('/create-point', (req, res) => {
- return res.render('create-point.html')
-})
-server.get('/search', (req, res) => {
- return res.render('search-results.html')
+//Configuração das rotas/caminhos da aplicação
+//Rota "Home", página inicial.
+//req: Requisição
+//res: Resposta
+server.get("/", (req, res) => { //Get é um verbo de http 
+    //Antes de instalar o nunjucks:
+    //res.sendFile(__dirname + "/views/index.html") //dirname é o nome do diretório
+    //Após instalação do nunjucks, trocamos por render e como já le arquivos html, deixamos como string:
+    return res.render("index.html", { title: "Um título" })//o "res" entende que irá passar pelo motor do nunjucks o index.html e irá retornar/o 2ªelement é 1 objeto    
 })
 
-// ligar o servidor 
+//Rota que irá receber dados do formulário, através do req /Create-point:
+server.get("/create-point", (req, res) => {
+    //req.query: Query Strings da nossa URL e consegue pegar as informações que estão la
+    //console.log(req.query)
+
+    //Antes de instalar o nunjucks:
+    //res.sendFile(__dirname + "/views/create-point.html")
+    //Após instalação do nunjucks, trocamos por render e como já le arquivos html, deixamos como string:
+    return res.render("create-point.html")
+})
+//Usando o verbo POST e trocamos o caminho para savepoint ao inves de /create-point
+server.post("/savepoint", (req, res) => {
+
+    // req.body: O corpo do nosso formulário
+    //console.log(req.body)
+
+    //Inserir dados no banco de dados
+    const query = `
+    INSERT INTO places (
+        image, 
+        name,
+        address,
+        address2,
+        state,
+        city,
+        items
+    ) VALUES (?,?,?,?,?,?,?);
+    `
+    const values = [
+        req.body.image,
+        req.body.name, 
+        req.body.address, 
+        req.body.address2, 
+        req.body.state, 
+        req.body.city, 
+        req.body.items 
+    ]
+
+    function afterInsertData(err) {
+        if(err) {
+            return console.log(err)
+        }
+        console.log("Cadastrado com sucesso")
+        console.log(this) 
+
+        return res.render("create-point.html", { saved: true})
+    }
+
+    db.run(query, values, afterInsertData)
+})
+
+//Rota /Search-results
+server.get("/search", (req, res) => {
+
+    //Ver pontos cadastrados
+    const search = req.query.search
+    if(search == "") {
+        //pesquisa vazia
+        return res.render("search-results.html", { total: 0})
+    }
+
+
+    //Pegar os dados do banco de dados
+    //Antes era: ity = '${search}', mas pega o valor correto digitado na cidade (tipo, Rio de Janeiro, se colocar só Rio não vai), então mudamos para "city LIKE '%${search}%'" pode vir um valor antes ou depois do 'Rio' que esta correto
+    db.all(`SELECT * FROM places WHERE city LIKE '%${search}%'`, function (err, rows) {
+        if (err) { //Esse é o tratamento do caminho do erro
+            console.log(err)
+            return res.send("Erro no cadastro!")
+        }
+        const total = rows.length
+        //Mostrar a página html com os dados do banco de dados
+        return res.render("search-results.html", { places: rows, total: total})
+    })
+    //return res.render("search-results.html")->colocamos dentro do código acima
+})
+
+//ligar o servidor
 server.listen(3000)
-console.log('http://localhost:3000/')
